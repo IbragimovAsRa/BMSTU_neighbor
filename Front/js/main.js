b81c7bd696d497f38abb3b5f705f8c27b11d064f
never91veup <vp.vlad00@mail.ru>
2022-09-23 22:48:43 +0300

    added effects

--- Front/js/main.js
+++ Front/js/main.js
@@ -1,7 +1,7 @@
 (function () {
   const show = document.getElementById('show');
   const ul = document.getElementById('posts');
-  const url = 'https://jsonplaceholder.typicode.com/posts';
+  const url = 'https://jsonplaceholder.typicode.com/posts?_limit=6';
 
   const showAdvertisments = () => {
     ul.innerHTML = '';
@@ -14,14 +14,21 @@
         const id = post.id;
 
         let li = createNode('li');
-        li.className += 'list__item'
+        li.className += 'list__item';
         let title = createNode('h2');
+        title.className += 'item__title';
+        let img = createNode('img');
+        img.className += 'item__img';
+        img.src = `img/info${id}.jpg`;
+        img.alt = 'Content';
         let body = createNode('p');
+        body.className += 'item__description';
         
         title.innerHTML = `${post.title}`;
         body.innerHTML = `${post.body}`;
   
         append(li, title);
+        append(li, img);
         append(li, body);
         append(ul, li);
       })
